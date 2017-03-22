518d8c7a206785947cd528b8cf72a935fa7d17d1
mikesankovich <misankovich@gmail.com>
2017-03-22 00:23:58 -0400

    coverage up

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,7 @@ describe('Validator', () => {
     expect(passwordObject.isValid).to.equal(true);
     expect(passwordObject.password).to.equal('tringtR2-re');
   });
+
   describe('Validate Password Type (no options)', () => {
     it('Should validate that the password is a string(no options)', () => {
       const passwordObject = pwdValidate('tringtR2-re');
@@ -19,8 +20,16 @@ describe('Validator', () => {
       expect(passwordObject.errorMessages.passwordType).to.equal('Password must be a string');
     });
   });
+
+  describe('Validate Options Type', () => {
+    it('Should fail if the password is not an object', () => {
+      const passwordObject = pwdValidate('123', 'bad');
+      expect(passwordObject.isValid).to.equal(false);
+      expect(passwordObject.errorMessages.optionsType).to.equal('Optional arguments must be passed as an object');
+    });
+  });
+
   describe('Validate Password with options', () => {
-    // const passwordObject = pwdValidate('tringtR2-re', {hasSpecialCharacter: true, hasNoInvalidStrings: ['true', 'myname']});
     it('Should validate min length', () => {
       const passwordObject = pwdValidate('trin', {minLength: 5});
 
@@ -30,6 +39,12 @@ describe('Validator', () => {
 
     it('Should validate max length', () => {
       const passwordObject = pwdValidate('string', {maxLength: 5});
+      expect(passwordObject.isValid).to.equal(false);
+      expect(passwordObject.errorMessages.maxLength).to.equal('Password contains too many characters. Password contains 6 characters, and allows up to 5 characters');
+    });
+
+    it('Should fail if minLength >= maxLength', () => {
+      const passwordObject = pwdValidate('string', {maxLength: 5, minLength:7});
       expect(passwordObject.isValid).to.equal(false);
       expect(passwordObject.errorMessages.maxLength).to.equal('Password contains too many characters. Password contains 6 characters, and allows up to 5 characters');
     });
@@ -62,6 +77,22 @@ describe('Validator', () => {
       const passwordObject = pwdValidate('truedat', {hasNoInvalidStrings: ['true', 'myname']});
       expect(passwordObject.isValid).to.equal(false);
       expect(passwordObject.errorMessages.hasNoInvalidStrings).to.equal('Password cannot contain the following strings: [true, myname]');
+    });
+
+    it('Should fail if hasNoInvalidStrings is not an array', () => {
+      try {
+        pwdValidate('truedat', {hasNoInvalidStrings: 'myname'});
+      } catch (e) {
+        expect(e.message).to.equal('hasNoInvalidStrings must be an array');
+      }
+    });
+
+    it('Should fail if the wrong datatype is passed to options', () => {
+      try {
+        pwdValidate('truedat', {minLength: 'asdf'});
+      } catch (e) {
+        expect(e.message).to.equal('minLength must be a number');
+      }
     });
 
   });
